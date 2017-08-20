16f6a54fca544ec48011af72fb928fd842067d1a
Shreyas <shrysghnt@gmail.com>
2017-08-19 22:24:43 -0400

    detect textarea success

--- flood.py
+++ flood.py
@@ -10,9 +10,12 @@ driver.get(('https://shrysg.sarahah.com/'))
 
 text = 'test_text'
 
-textArea = driver.find_elements_by_tag_name('textarea')
 
-textArea.send_keys("This text is send using Python code.")
+driver.find_element_by_xpath(".//*[@id='Text']").send_keys(text)
+
+#textArea = driver.find_elements_by_tag_name('textarea')
+#driver.find_elements_by_tag_name('textarea').send_keys('test1')
+#textArea.send_keys("This text is send using Python code.")
 
 #String textagain = text.getAttribute("value");
 #username.send_keys(usernameStr)
