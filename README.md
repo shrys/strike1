19e8478f6d474b1995803e30c97616d4656454d3
Shreyas <shrysghnt@gmail.com>
2017-08-20 01:22:39 -0400

    added sentence generating library

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # strike1
+
+used https://github.com/hrs/markov-sentence-generator for generating sentences
--- flood.py
+++ flood.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import webbrowser
 import os
 import time
@@ -7,13 +9,21 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import gen
 
 driver = webdriver.Firefox()
 for i in range(0,2):
-	digits = "".join( [random.choice(string.digits) for i in xrange(8)] )
-	chars = "".join( [random.choice(string.letters) for i in xrange(15)] )
+	#digits = "".join( [random.choice(string.digits) for i in xrange(8)] )
+	#chars = "".join( [random.choice(string.letters) for i in xrange(15)] )
 	driver.get(('https://shrysg.sarahah.com/'))
-	text = digits + chars
+	#text = digits + chars
+	filename = "snippet.txt"
+	markovLength = 5
+	gen.buildMapping(gen.wordlist(filename), markovLength)
+	text = gen.genSentence(markovLength)
+
 	driver.find_element_by_xpath(".//*[@id='Text']").send_keys(text)
 	driver.find_element_by_xpath(".//*[@id='Send']").click()
 	time.sleep(5)
+
+
