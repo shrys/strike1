3e3b63b059be6f746dc4bbbf723e824fb40a496a
Shreyas <shrysghnt@gmail.com>
2017-08-19 22:57:17 -0400

    subdomain removed

--- flood.py
+++ flood.py
@@ -6,18 +6,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
-driver.get(('https://shrysg.sarahah.com/'))
-
-text = 'test_text'
-
-
-driver.find_element_by_xpath(".//*[@id='Text']").send_keys(text)
-
-#textArea = driver.find_elements_by_tag_name('textarea')
-#driver.find_elements_by_tag_name('textarea').send_keys('test1')
-#textArea.send_keys("This text is send using Python code.")
-
-#String textagain = text.getAttribute("value");
-#username.send_keys(usernameStr)
-#print (text)
-
+for i in range(0,2):
+	driver.get(('https://.sarahah.com/'))
+	text = 'test_text'
+	driver.find_element_by_xpath(".//*[@id='Text']").send_keys(text)
+	driver.find_element_by_xpath(".//*[@id='Send']").click()
