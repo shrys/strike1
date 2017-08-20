a73c0eabfe1d4dc9d53773bd9ae3df16bbca8447
Shreyas <shrysghnt@gmail.com>
2017-08-20 01:28:20 -0400

    changed limits

--- flood.py
+++ flood.py
@@ -12,13 +12,14 @@ from selenium.webdriver.support import expected_conditions as EC
 import gen
 
 driver = webdriver.Firefox()
-for i in range(0,2):
+for i in range(0,50):
 	#digits = "".join( [random.choice(string.digits) for i in xrange(8)] )
 	#chars = "".join( [random.choice(string.letters) for i in xrange(15)] )
-	driver.get(('https://shrysg.sarahah.com/'))
+	driver.get(('https://.sarahah.com/'))
 	#text = digits + chars
 	filename = "snippet.txt"
-	markovLength = 5
+	
+	markovLength = 5 ##markov length
 	gen.buildMapping(gen.wordlist(filename), markovLength)
 	text = gen.genSentence(markovLength)
 
